9104b3f7c8e1481c2d7b9e9c6b9957bcafaa1626
mark <mchellmer@gmail.com>
2018-08-11 07:17:55 +0000

    Model populated with example code

---
+++ model/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-1_KbY1C6kyS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3hc1p140bki8hfjnnpsg8smne2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
